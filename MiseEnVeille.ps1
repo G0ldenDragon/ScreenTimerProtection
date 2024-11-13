44d12a3f66a00d0a9f479b8abdb5a39be10e199a
G0ldenDragon <149322207+G0ldenDragon@users.noreply.github.com>
2024-11-13 20:37:47 +0100

    Script de Mise en Veille automatique déjà peaufiné et fini.

---
+++ MiseEnVeille.ps1
@@ -0,0 +1,80 @@
+﻿# Import des classes
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+# Récupération de l'ID du processus actuel
+$currentProcessId = $PID
+
+# Recherche et arrêt de l'ancien script MiseEnVeille.ps1, en excluant le processus actuel
+Get-CimInstance Win32_Process -Filter "Name='powershell.exe' OR Name='pwsh.exe'" |
+    Where-Object { 
+        $_.CommandLine -like "*MiseEnVeille.ps1*" -and $_.ProcessId -ne $currentProcessId 
+        } |
+        ForEach-Object { 
+            Stop-Process -Id $_.ProcessId -Force
+            Write-Host "Un ancien script MiseEnVeille.ps1 (Process ID: $($_.ProcessId)) a été arrêté."
+        }
+
+Write-Host "Le script actuel MiseEnVeille.ps1 (Process ID: $currentProcessId) en cours d'exécution."
+
+# 1h - 5 mins = 55 mins
+$secondes = 59 * 60
+# Mise en attente
+Start-Sleep -Seconds $secondes
+
+# Initialisations
+$veille = $false
+$secondes = 1 * 60
+# -ne : != (différent de) ; -eq : = (égal à)
+while ($veille -eq $false)
+{
+    Write-Host "--- Fin de l'attente. ---"
+    # Son jouer en boucle
+    $soundFile = "Alarm01.wav"
+
+    # Création du lecteur audio
+    $player = New-Object System.Media.SoundPlayer
+    $player.SoundLocation = $soundFile
+
+    # Jouer le son en boucle
+    $player.PlayLooping()
+
+    # Affichage de la boîte de message avec les boutons "Oui" et "Non"
+    $response = [System.Windows.Forms.MessageBox]::Show(
+        # Message à afficher
+        "Retarder la mise en veille qui aura lieu dans 1 mins ?",
+
+        # Titre de la boîte de dialogue
+        "Mise en veille automatique dans peu de temps !",
+
+        [System.Windows.Forms.MessageBoxButtons]::YesNo, # Boutons à afficher
+        [System.Windows.Forms.MessageBoxIcon]::Warning  # Icône à afficher
+    )
+
+    # Vérification de la réponse de l'utilisateur
+    if ($response -eq [System.Windows.Forms.DialogResult]::Yes) 
+    {
+        Write-Host "Mise en veille retardée..."
+    } 
+    else 
+    {
+        Write-Host "Mise en veille dans 1 minutes..."
+        $veille = $true
+    }
+
+    # Une fois la boîte de message fermée, arrêter le son
+    $player.Stop()
+    Start-Sleep -Seconds $secondes
+}
+
+# Prévention que ce sont les 15 dernières secondes.
+Write-Host "Mise en veille dans 15 secondes..."
+$soundFile = "ALARM.wav"
+$player = New-Object System.Media.SoundPlayer
+$player.SoundLocation = $soundFile
+$player.PlayLooping()
+Start-Sleep -Seconds 15
+$player.Stop()
+
+# Mise en veille de l'ordinateur.
+rundll32.exe powrprof.dll,SetSuspendState 0,1,0
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ScreenTimerProtection
 Un script PowerShell permettant de mettre automatiquement en veille au bout d'une heure l'ordinateur.
+
+# Explications détaillées
+Ce script, une fois démarrer va kill tous script de même nom puis se mettre en pause pendant X minutes (ici 59 minutes).
+Une fois ces minutes passées, une fenêtre de choix Windows va apparaître pour donner le choix à l'utilisateur de repoussé de X minutes (ici 1 minute) la mise en veille de l'ordinateur.
+Tant que le choix sur cette fenêtre n'est pas effectuer, un son sera jouer (ici Alarm01.wav, l'alarme de base de Windows).
+
+- Si l'utilisateur choisi de repousser la mise en veille, 1 minutes plus tard, cette même fenêtre s'ouvrira.
+- Si l'utilisateur ne choisi pas de repousser la mise en veille, 1 minutes plus tard, un son sera jouer (ici ALARM.wav récupérer d'un site de son libre de droit) pendant 15 secondes pour prévenir la mise en veille imminente.
+
+Puis l'ordinateur est mis en veille.
+
+# Conseils d'utilisation
+Le mieux est de faire en sorte que ce script s'active automatiquement en arrière plan (sans que cela puisse être visible) lorsque l'ordinateur démarre et lorsque l'ordinateur sort de la veille. 
+
+Par exemple : 
+- pour le démarrage de l'ordinateur, il suffit de placer le raccourci (.Ink) dans le dossier Démarrage (Startup en anglais) de Windows.
+- pour la sortie de mise en veille de l'ordinateur, il suffit de créer une tâche dans taskschd.msc et importer l'XML.
